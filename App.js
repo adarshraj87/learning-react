9512e308b98b24ef6b1e0a4340925ce10fa418e6
adarshraj87 <adarshraj59@gmail.com>
2017-07-12 20:43:52 +0530

    basic props

--- App.js
+++ App.js
@@ -1,43 +1,32 @@
 import React, {Component} from "react";
+
 import {AppRegistry, StyleSheet, Text, View} from "react-native";
 
-class StateTypes extends Component {
 
-
-    constructor() {
-        super();
-        this.state = {
-            name: 'Adarsh Raj',
-            langauges: ['PHP', 'Node.js'],
-            Project: {
-                'name': "Learning React"
-            }
-        }
-    }
+class Language extends Component {
 
     render() {
         return (
             <View style={styles.container}>
-                <Text>{this.state.name}</Text>
-                <Text>{this.state.langauges.length}</Text>
-                <Text>{this.state.Project.name}</Text>
+                <DisplayLanguage name="Node.js"/>
             </View>
         )
     }
-
 }
 
+export class DisplayLanguage extends Component {
+    render() {
+        return (<Text>{this.props.name}</Text>)
+    }
+}
+export default Language;
+
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
+    container: {
+        flex: 1,
+        justifyContent: 'center',
         alignItems:'center'
     }
 })
 
-
-
-
-export default StateTypes;
-
-AppRegistry.registerComponent('StateType', () => StateTypes);
+AppRegistry.registerComponent('Language', () => Language);
